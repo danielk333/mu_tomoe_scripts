451190caafcda79430c01643ea1668d793ec0072
danielk <daniel.kastinen@irf.se>
2019-03-01 06:30:19 +0100

    added mu gain and rcs calcs

---
+++ calc_rcs.py
@@ -0,0 +1,89 @@
+import numpy as n
+import scipy.constants as const
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+import dpt
+
+#import mu stuff
+from mu_conf import *
+from mu_gain import *
+
+
+def rcs(SNR, R, rx_T, G_tx, G_rx, wavelength, rx_bw, tx_power):
+    c1 = SNR*(4.0*n.pi)**3*R**4*const.k*rx_T*rx_bw
+    c2 = tx_power*G_tx*G_rx*wavelength**2
+    return c1/c2
+
+def MU_rcs(SNR, r, T_cos = 10000.0):
+    G = MU_gain(r)
+    rx_T = 3000.0 + T_cos
+    return rcs(SNR, n.linalg.norm(r), rx_T, G, G, lambda0, bw0, tx_P)
+
+
+if __name__ == '__main__':
+
+    tomoe_part = n.genfromtxt('data/tomoe_part.csv')
+
+    #FOR DEBUG, JUST DO ONE
+    tomoe_part = tomoe_part[:1,:]
+
+    cart_shift = n.array([0,0,100e3],dtype=n.float)
+
+    az0, el0, r0 = dpt.convert.cart_to_azel(cart_shift[0], cart_shift[1], cart_shift[2], radians=False)
+    start = dpt.convert.azel_to_azel_via_ecef(
+    mu_lat,mu_lon,mu_alt,
+    az0,el0,r0,
+    kiso_lat,kiso_lon,kiso_alt)
+    dir_kiso = dpt.convert.azel_to_cart(n.radians(start[0]), n.radians(start[1]), start[2])
+
+    plot_beam = True
+    res = 100
+    rho = n.linspace(0, 1, endpoint=True, num=res, dtype=n.float)
+
+    for j in range(tomoe_part.shape[0]):
+        cart0t = dpt.convert.azel_to_cart(n.radians(tomoe_part[j, 0]), n.radians(tomoe_part[j, 1]), tomoe_part[j, 2])
+        cart1t = dpt.convert.azel_to_cart(n.radians(tomoe_part[j, 3]), n.radians(tomoe_part[j, 4]), tomoe_part[j, 5])
+
+        cart0tn = cart0t/n.linalg.norm(cart0t)
+        cart0tn[2] += 10.0*n.log10(I_0) + 3.0
+        cart1tn = cart1t/n.linalg.norm(cart1t)
+        cart1tn[2] += 10.0*n.log10(I_0) + 3.0
+
+        gain_curve = n.empty(rho.shape,dtype=n.float)
+        snr_curve = n.full(rho.shape, 10.0, dtype=n.float)
+        rcs_curve = n.empty(rho.shape,dtype=n.float)
+        for ind in range(res):
+            r = cart0t*(1.0 - rho[ind]) + cart1t*rho[ind]
+            gain_curve[ind] = MU_gain(r)
+            rcs_curve[ind] = MU_rcs(snr_curve[ind], r)
+
+        if plot_beam:
+            fig = plt.figure(figsize=(14,12))
+
+            ax = fig.add_subplot(231)
+            ax.plot(rho, 10.0*n.log10(gain_curve))
+            ax.set_xlabel('Trajectory')
+            ax.set_ylabel('Gain [dB]')
+
+            ax = fig.add_subplot(232)
+            ax.plot(rho, 10.0*n.log10(snr_curve))
+            ax.set_xlabel('Trajectory')
+            ax.set_ylabel('SNR [dB]')
+
+            ax = fig.add_subplot(233)
+            ax.plot(rho, 10.0*n.log10(rcs_curve*100.0**2))
+            ax.set_xlabel('Trajectory')
+            ax.set_ylabel('RCS [dB$cm^2$]')
+
+            ax = fig.add_subplot(212, projection='3d')
+            add_ax_gain3d(ax, 50, 80.0)
+            ax.plot([cart0tn[0], cart1tn[0]], [cart0tn[1], cart1tn[1]], [cart0tn[2], cart1tn[2]], '.-r')
+            ax.set_xlabel('$k_x$ [1]')
+            ax.set_ylabel('$k_y$ [1]')
+            ax.set_zlabel('Gain $G$ [dB]')
+
+            fig.savefig('img/test_rcs_curve_calc',bbox_inches='tight')
+            plt.show()
+
+
---
+++ mu_gain.py
@@ -0,0 +1,67 @@
+import numpy as n
+import scipy.constants as const
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
+from mu_conf import *
+
+def plane_wave(k,r,p):
+    return n.exp(1j*n.pi*2.0*n.dot(k-p,r))
+
+def array(k, antennas, f, I_0):
+    I_scale = I_0/(antennas.shape[0]**2.0)
+    k = k/n.linalg.norm(k)
+    p = n.array([0,0,1], dtype=k.dtype)
+    G = n.exp(1j)*0.0
+    #r in meters, divide by lambda
+    for r in antennas:
+        G += plane_wave(k,r/(const.c/f),p)
+    #ugly fix, multiply gain by k_z to enmulate beam stering loss as a function of elevation
+    #should be antenna element gain pattern of k...
+    return n.abs(G.conj()*G*I_scale)*p[2]
+
+
+def MU_gain(k):
+    return array(k, MU_antennas, f0, I_0)
+
+def add_ax_gain3d(ax, res, min_el):
+    kx=n.linspace(-n.cos(min_el*n.pi/180.0),n.cos(min_el*n.pi/180.0),num=res)
+    ky=n.linspace(-n.cos(min_el*n.pi/180.0),n.cos(min_el*n.pi/180.0),num=res)
+    
+    S=n.zeros((res,res))
+    K=n.zeros((res,res,2))
+    for i,x in enumerate(kx):
+        for j,y in enumerate(ky):
+            z2 = x**2 + y**2
+            if z2 < n.cos(min_el*n.pi/180.0)**2:
+                k = n.array([x,y,n.sqrt(1.0-z2)], dtype=n.float)
+                S[i,j]=MU_gain(k)
+            else:
+                S[i,j] = 0;
+            K[i,j,0]=x
+            K[i,j,1]=y
+    SdB = n.log10(S)*10.0
+    SdB[SdB < -5.0] = -5.0
+    surf = ax.plot_surface(K[:,:,0],K[:,:,1],SdB,cmap=cm.plasma, linewidth=0, antialiased=False, vmin=0, vmax=n.max(SdB))
+
+
+def plot_gain3d(res=200,min_el = 0.0):
+    plt.rc('text', usetex=True)
+    
+    fig = plt.figure(figsize=(15,7))
+    ax = fig.add_subplot(111, projection='3d')
+    
+    add_ax_gain3d(ax, res, min_el)
+    ax.set_xlabel('$k_x$ [1]',fontsize=24)
+    ax.set_ylabel('$k_y$ [1]',fontsize=24)
+    ax.set_zlabel('Gain $G$ [dB]',fontsize=24)
+    plt.xticks(fontsize=17)
+    plt.yticks(fontsize=17)
+    ax.set_title('Gain pattern MU', fontsize=28)
+    plt.show()
+
+
+if __name__=='__main__':
+    pass
+
